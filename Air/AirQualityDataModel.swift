b51fb8202e31b1deb93b24519215da5e97775bde
Marcin Szabłowski <Marcin@MacBook-Air-Marcin.local>
2018-01-25 21:17:07 +0100

    Add JSON parsing

---
+++ Air/AirQualityDataModel.swift
@@ -0,0 +1,35 @@
+//
+//  AirQualityDataModel.swift
+//  Air
+//
+//  Created by Marcin Szabłowski on 25.01.2018.
+//  Copyright © 2018 Marcin Szabłowski. All rights reserved.
+//
+
+import UIKit
+
+class AirQualityDataModel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
